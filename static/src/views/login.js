21d1e816227d225f302a7e282cb909e4cc23301c
unriellistic <charliewindle@gmail.com>
2022-10-20 14:05:45 +0800

    added pristine.js form validation

--- static/src/views/login.js
+++ static/src/views/login.js
@@ -2,6 +2,7 @@ import m from "mithril";
 import $ from "cash-dom";
 
 import Navbar from "../components/navbar";
+import Pristine from "../libraries/pristine";
 
 var Login = {
     view: () => (
@@ -11,26 +12,26 @@ var Login = {
                 <div class="block">
                     <h1>Log In</h1>
                 </div>
-                <div class="block">
+                <form class="block" id="loginForm">
                     <input type="hidden" id="csrf_token"></input>
                     <div class="field">
                         <label class="label">Username</label>
                         <div class="control">
-                            <input class="input" type="text" id="usernameInput"></input>
+                            <input class="input" type="text" id="usernameInput" required></input>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Password</label>
                         <div class="control">
-                            <input class="input" type="password" id="passwordInput"></input>
+                            <input class="input" type="password" id="passwordInput" required></input>
                         </div>
                     </div>
                     <div class="field">
                         <div class="control">
-                            <button class="button is-link" id="loginButton">Log In</button>
+                            <input type="submit" class="button is-link" id="signupButton" value="Log In"></input>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     ),
@@ -41,25 +42,41 @@ var Login = {
         }).then(response => {
             $("#csrf_token").val(response.csrf_token);
         });
-        $("#loginButton").on("click", () => {
-            console.log("test");
-            m.request({
-                method: "POST",
-                url: "/login",
-                body: {
-                    "_csrf_token": $("#csrf_token").val(),
-                    "username": $("#usernameInput").val(),
-                    "password": $("#passwordInput").val()
-                }
-            }).then(response => {
-                alert(response.msg);
-                localStorage.setItem('jwt', response.access_token); // log in the user
-                location.href = "#!/home" // redirect to homepage
-            }).catch(e => {
-                if (e.code === 401) { // unauthorized - probably invalid username/password
-                    alert(e.response.msg);
-                }
-            })
+
+        var form = document.getElementById("loginForm");
+
+        var pristine = new Pristine(form, {
+            classTo: "field",
+            errorClass: "is-danger",
+            successClass: "is-success",
+            errorTextParent: "field",
+            errorTextTag: "p",
+            errorTextClass: "help is-danger"
+        });
+
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+
+            var valid = pristine.validate();
+            if (valid) {
+                m.request({
+                    method: "POST",
+                    url: "/login",
+                    body: {
+                        "_csrf_token": $("#csrf_token").val(),
+                        "username": $("#usernameInput").val(),
+                        "password": $("#passwordInput").val()
+                    }
+                }).then(response => {
+                    alert(response.msg);
+                    localStorage.setItem('jwt', response.access_token); // log in the user
+                    location.href = "#!/home" // redirect to homepage
+                }).catch(e => {
+                    if (e.code === 401) { // unauthorized - probably invalid username/password
+                        alert(e.response.msg);
+                    }
+                });
+            }
         });
     }
 }
--- static/src/views/signup.js
+++ static/src/views/signup.js
@@ -1,7 +1,6 @@
 import m from "mithril";
 import $ from "cash-dom";
-import validator from "email-validator";
-import Pristine from "pristine";
+import Pristine from "../libraries/pristine";
 
 import Navbar from "../components/navbar";
 
@@ -13,42 +12,38 @@ var Signup = {
                 <div class="block">
                     <h1>Sign Up</h1>
                 </div>
-                <div class="block">
+                <form class="block" id="signupForm">
                     <input type="hidden" id="csrf_token"></input>
                     <div class="field">
                         <label class="label">Username</label>
                         <div class="control">
-                            <input class="input" type="text" id="usernameInput"></input>
-                            <p class="help is-danger" id="usernameHelp"></p>
+                            <input class="input" type="text" id="usernameInput" required></input>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Email</label>
                         <div class="control">
-                            <input class="input" type="email" id="emailInput"></input>
-                            <p class="help is-danger" id="emailHelp"></p>
+                            <input class="input" type="email" id="emailInput" required></input>
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Password</label>
                         <div class="control">
-                            <input class="input" type="password" id="passwordInput"></input>
+                            <input class="input" type="password" id="passwordInput" required minlength="8"></input>
                         </div>
-                        <p class="help is-danger" id="passwordHelp"></p>
                     </div>
                     <div class="field">
                         <label class="label">Confirm Password</label>
                         <div class="control">
-                            <input class="input" type="password" id="confirmPasswordInput"></input>
+                            <input class="input" type="password" id="confirmPasswordInput" required minlength="8" data-pristine-equals="#passwordInput"></input>
                         </div>
-                        <p class="help is-danger" id="confirmPasswordHelp"></p>
                     </div>
                     <div class="field">
                         <div class="control">
-                            <button class="button is-link" id="signupButton">Sign Up</button>
+                            <input type="submit" class="button is-link" id="signupButton" value="Sign Up"></input>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     ),
@@ -60,78 +55,40 @@ var Signup = {
             $("#csrf_token").val(response.csrf_token);
         });
 
-        $("#usernameInput").on("blur change", () => {
-            if ($("#usernameInput").val().length == 0) {
-                $("#usernameInput").addClass("is-danger");
-                $("#usernameHelp").text("Please enter a username.");
-            } else {
-                $("#usernameInput").removeClass("is-danger");
-                $("#usernameHelp").text("");
-            }
+        var form = document.getElementById("signupForm");
+
+        var pristine = new Pristine(form, {
+            classTo: "field",
+            errorClass: "is-danger",
+            successClass: "is-success",
+            errorTextParent: "field",
+            errorTextTag: "p",
+            errorTextClass: "help is-danger"
         });
 
-        $("#emailInput").on("blur change", () => {
-            if ($("#emailInput").val().length == 0) {
-                $("#emailInput").addClass("is-danger");
-                $("#emailHelp").text("Please enter an email.");
-            } else if (!validator.validate($("#emailInput").val())) {
-                $("#emailInput").addClass("is-danger");
-                $("#emailHelp").text("Please enter a valid email address.");
-            } else {
-                $("#emailInput").removeClass("is-danger");
-                $("#emailHelp").text("");
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+
+            var valid = pristine.validate();
+            if (valid) {
+                m.request({
+                    method: "POST",
+                    url: "/signup",
+                    body: {
+                        "_csrf_token": $("#csrf_token").val(),
+                        "username": $("#usernameInput").val(),
+                        "email": $("#emailInput").val(),
+                        "password": $("#passwordInput").val()
+                    }
+                }).then(response => {
+                    alert(response.msg);
+                    location.href = "#!/login" // redirect after successful signup
+                }).catch(e => {
+                    if (e.code === 401) { // unauthorized - probably user already exists
+                        alert(e.response.msg);
+                    }
+                });
             }
-        });
-
-        $("#passwordInput").on("blur change", () => {            
-            if ($("#passwordInput").val().length == 0) {
-                $("#passwordInput").addClass("is-danger");
-                $("#passwordHelp").text("Please enter a password.");
-            } else if ($("#passwordInput").val().length < 8) {
-                $("#passwordInput").addClass("is-danger");
-                $("#passwordHelp").text("Minimum 8 characters.");
-            } else {
-                $("#passwordInput").removeClass("is-danger");
-                $("#passwordHelp").text("");
-            }
-        });
-
-        $("confirmPasswordInput").on("blur change", () => {
-            if ($("#confirmPasswordInput").val().length == 0) {
-                $("#confirmPasswordInput").addClass("is-danger");
-                $("#confirmPasswordHelp").text("Please enter a password.");
-            } else if ($("#confirmPasswordInput").val() !== $("#passwordInput").val()) {
-                $("#confirmPasswordInput").addClass("is-danger");
-                $("#confirmPasswordHelp").text("Passwords do not match.");
-            } else {
-                $("#confirmPasswordInput").removeClass("is-danger");
-                $("#confirmPasswordHelp").text("");
-            }
-        });
-
-        $("#signupButton").on("click", () => {
-            $("#usernameInput").blur();
-            $("#emailInput").blur();
-            $("#passwordInput").blur();
-            $("#confirmPasswordInput").blur();
-
-            m.request({
-                method: "POST",
-                url: "/signup",
-                body: {
-                    "_csrf_token": $("#csrf_token").val(),
-                    "username": $("#usernameInput").val(),
-                    "email": $("#emailInput").val(),
-                    "password": $("#passwordInput").val()
-                }
-            }).then(response => {
-                alert(response.msg);
-                location.href = "#!/login" // redirect after successful signup
-            }).catch(e => {
-                if (e.code === 401) { // unauthorized - probably user already exists
-                    alert(e.response.msg);
-                }
-            })
         });
     }
 }
