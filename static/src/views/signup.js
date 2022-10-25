285d749bce472d7b472b2ad07a94c34357bede4a
unriellistic <charliewindle@gmail.com>
2022-10-25 21:29:28 +0800

    added basic framework for additional signup fields

--- static/src/views/signup.js
+++ static/src/views/signup.js
@@ -1,5 +1,4 @@
 import m from "mithril";
-import $ from "cash-dom";
 import Pristine from "../libraries/pristine";
 
 import Navbar from "../components/navbar";
@@ -14,28 +13,60 @@ var Signup = {
                 </div>
                 <form class="block" id="signupForm">
                     <input type="hidden" id="csrf_token"></input>
-                    <div class="field">
-                        <label class="label">Username</label>
-                        <div class="control">
-                            <input class="input" type="text" id="usernameInput" required></input>
+                    <div class="columns is-mobile is-multiline">
+                        <div class="field column is-half pt-0">
+                            <label class="label">Username</label>
+                            <div class="control">
+                                <input class="input" type="text" id="usernameInput" required></input>
+                            </div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Email</label>
-                        <div class="control">
-                            <input class="input" type="email" id="emailInput" required></input>
+                        <div class="field column is-half pt-0">
+                            <label class="label">Email</label>
+                            <div class="control">
+                                <input class="input" type="email" id="emailInput" required></input>
+                            </div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <div class="control">
-                            <input class="input" type="password" id="passwordInput" required minlength="8"></input>
+                        <div class="field column is-half pt-0">
+                            <label class="label">Password</label>
+                            <div class="control">
+                                <input class="input" type="password" id="passwordInput" required minlength="8"></input>
+                            </div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Confirm Password</label>
-                        <div class="control">
-                            <input class="input" type="password" id="confirmPasswordInput" required minlength="8" data-pristine-equals="#passwordInput"></input>
+                        <div class="field column is-half pt-0">
+                            <label class="label">Confirm Password</label>
+                            <div class="control">
+                                <input class="input" type="password" id="confirmPasswordInput" required minlength="8" data-pristine-equals="#passwordInput"></input>
+                            </div>
+                        </div>
+                        <div class="field column is-one-third pt-0">
+                            <label class="label">Gender</label>
+                            <div class="control">
+                                <div class="select">
+                                    <select id="genderInput" required>
+                                        <option>---</option>
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                        <option>Other</option>
+                                        <option>Prefer not to say</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field column is-one-third pt-0">
+                            <label class="label">Country</label>
+                            <div class="control">
+                                <div class="select">
+                                    <select id="countryInput" required></select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field column is-one-third pt-0">
+                            <label class="label">Region</label>
+                            <div class="control">
+                                <div class="select">
+                                    <select id="regionInput" required></select>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="field">
@@ -78,7 +109,10 @@ var Signup = {
                         "_csrf_token": $("#csrf_token").val(),
                         "username": $("#usernameInput").val(),
                         "email": $("#emailInput").val(),
-                        "password": $("#passwordInput").val()
+                        "password": $("#passwordInput").val(),
+                        "gender": $("#genderInput option:selected").text(),
+                        "country": $("#countryInput option:selected").text(),
+                        "region": $("#regionInput option:selected").text()
                     }
                 }).then(response => {
                     alert(response.msg);
