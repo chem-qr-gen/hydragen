e056ee7f8cf21c1a1c52437e5292066671f9759b
unriellistic <charliewindle@gmail.com>
2022-11-08 13:15:39 +0800

    "fixed" landing page

--- static/src/views/landing.js
+++ static/src/views/landing.js
@@ -6,18 +6,15 @@ var Landing = {
     view: () => (
         <div class="content">
             <Navbar />
-            <div class="hero is-fullheight-with-navbar" id="landing-splash">
+            <div class="hero is-large" id="landing-splash">
                 <div class="hero-body">
                     <p class="title">Mass Spectrometry Practice Made Easy</p>
                     <div class="block">
-                        <a class="button is-primary" href="#!/signup">Sign Up</a>
-                        <a class="button" href="#!/login">Log In</a>
-                        <a class="button" href="#!/home">Try Now</a>
+                        <a class="button is-primary mx-1" href="#!/signup">Sign Up</a>
+                        <a class="button mx-1" href="#!/login">Log In</a>
+                        <a class="button mx-1" href="#!/home">Try Now</a>
                     </div>
                 </div>
-            </div>
-            <div class="container">
-                <h1>test</h1>
             </div>
         </div>
     )
