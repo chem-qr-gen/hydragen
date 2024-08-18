e6ee0efec2686f24ea2c92e7c4d02d198d6b6e98
siewjh <jinghongsiew@gmail.com>
2024-08-18 18:50:45 +0800

    added leaderboard
    added leaderboard
    css not included

---
+++ chemquest_website/routes/get_leaderboard.py
@@ -0,0 +1,16 @@
+from flask_jwt_extended import jwt_required
+from flask import jsonify
+from sqlalchemy import select
+from chemquest_website import app, engine, meta
+import json
+
+@app.route('/get_leaderboard', methods=['GET'])
+@jwt_required(optional=True)
+def get_leaderboard():
+    # Return all users and their ratings
+    users_table = meta.tables["users"]
+    with engine.connect() as conn:
+        users_by_rating = conn.execute(select(users_table.c.username, users_table.c.elo)).fetchall()
+    users_by_rating = [user._asdict() for user in users_by_rating]
+
+    return jsonify(users_by_rating)
--- chemquest_website/static/src/views/leaderboard.js
+++ chemquest_website/static/src/views/leaderboard.js
@@ -6,17 +6,29 @@ var Leaderboard = {
     view: () => (
          <div class="content">
             <Navbar/>
-            <div class="container-wrapper">
+            <div class="unbounded-wrapper">
                 <div class="container">
                     <div class="block leaderboard-block">
                         <h1>Leaderboard</h1>
+                        <ol id="leaderboard-list"></ol>
                     </div>
                 </div>
             </div>
          </div>
     ),
-    oncreate: () => {
 
+    oncreate: () => {
+        m.request({
+            method: "GET",
+            url: "/get_leaderboard"
+        }).then(response => { // add in the leaderboard entries based on decreasing elo
+            let users_by_rating = response;
+            users_by_rating = users_by_rating.sort((a, b) => b.elo - a.elo);
+            let user;
+            for (user of users_by_rating){
+                $("#leaderboard-list").append("<li><span>" + user.username + "</span><span>" + user.elo + "</span></li>")
+            }
+        })
     }
 }
 
