fc08b4974c65c76a5927e247cd25a16aea650927
siewjh <jinghongsiew@gmail.com>
2024-08-18 21:43:33 +0800

    did the leaderboard ui
    used a table for the leaderboard, looks better now

--- chemquest_website/static/src/views/leaderboard.js
+++ chemquest_website/static/src/views/leaderboard.js
@@ -10,7 +10,16 @@ var Leaderboard = {
                 <div class="container">
                     <div class="block leaderboard-block">
                         <h1>Leaderboard</h1>
-                        <ol id="leaderboard-list"></ol>
+                        <table id="leaderboard-table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Rank</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Rating</th>
+                                </tr>
+                            </thead>
+                            <tbody id="leaderboard-tbody"></tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -21,12 +30,19 @@ var Leaderboard = {
         m.request({
             method: "GET",
             url: "/get_leaderboard"
-        }).then(response => { // add in the leaderboard entries based on decreasing elo
+        }).then(response => {
+            // add in the leaderboard entries based on decreasing elo
             let users_by_rating = response;
             users_by_rating = users_by_rating.sort((a, b) => b.elo - a.elo);
-            let user;
-            for (user of users_by_rating){
-                $("#leaderboard-list").append("<li><span>" + user.username + "</span><span>" + user.elo + "</span></li>")
+
+            for (let id = 0; id < users_by_rating.length; id++){
+                let user = users_by_rating[id];
+                $("#leaderboard-tbody").append(
+                    $("<tr>")
+                        .append($("<th>").attr("scope", "row").text(id + 1))
+                        .append($("<td>").text(user.username))
+                        .append($("<td>").text(Math.round((user.elo + Number.EPSILON) * 100) / 100)) // round to 2 digits (without trailing zeroes)
+                );
             }
         })
     }
