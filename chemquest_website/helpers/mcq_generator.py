32a3e31e7ae937d160989c2393600e6af73815f5
unriellistic <charliewindle@gmail.com>
2024-09-26 12:00:42 +0800

    important fixes for mcq generation algorithm

--- chemquest_website/helpers/mcq_generator.py
+++ chemquest_website/helpers/mcq_generator.py
@@ -59,24 +59,27 @@ def generate_option_alcohol(mol: Chem.Mol) -> list[dict]:
         return []
     
     # replace alcohol with a fluoride
-    halide_mols = rdmolops.ReplaceSubstructs(mol, funcgroups_mols.alcohol, Chem.MolFromSmiles("F"))
+    rxn1 = rdChemReactions.ReactionFromSmarts("[C:1][O:2]>>[C:1][F:2]")
+    products1 = rxn1.RunReactants((mol,))
     options = [
-        {"smiles": Chem.MolToSmiles(m), "explanation": ["alcohol-halide"]}
-         for m in halide_mols
+        {"smiles": Chem.MolToSmiles(m[0]), "explanation": ["alcohol-halide"]}
+         for m in products1
     ]
 
     # replace alcohol with an amine
-    amine_mols = rdmolops.ReplaceSubstructs(mol, funcgroups_mols.alcohol, Chem.MolFromSmiles("N"))
+    rxn2 = rdChemReactions.ReactionFromSmarts("[C:1][O:2]>>[C:1][N:2]")
+    products2 = rxn2.RunReactants((mol,))
     options += [
-        {"smiles": Chem.MolToSmiles(m), "explanation": ["alcohol-amine"]}
-         for m in amine_mols
+        {"smiles": Chem.MolToSmiles(m[0]), "explanation": ["alcohol-amine"]}
+         for m in products2
     ]
 
     # add methyl to form an ether
-    ether_mols = rdmolops.ReplaceSubstructs(mol, funcgroups_mols.alcohol, Chem.MolFromSmiles("OC"))
+    rxn3 = rdChemReactions.ReactionFromSmarts("[C:1][O:2]>>[C:1][O:2]C")
+    products3 = rxn3.RunReactants((mol,))
     options += [
-        {"smiles": Chem.MolToSmiles(m), "explanation": ["alcohol-ether"]}
-         for m in ether_mols
+        {"smiles": Chem.MolToSmiles(m[0]), "explanation": ["alcohol-ether"]}
+         for m in products3
     ]
 
     return options
@@ -100,20 +103,20 @@ def generate_option_primary_o(mol: Chem.Mol) -> list[dict]:
 
 
 def generate_option_primary_amine(mol: Chem.Mol) -> list[dict]:
-    amine_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("NH2"))
+    amine_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("[N;H2;X3]"))
 
     if not amine_matches:
         return []
     
     # replace amine with an alcohol
-    alcohol_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("NH2"), Chem.MolFromSmiles("O"))
+    alcohol_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("[N;H2;X3]"), Chem.MolFromSmiles("O"))
     options = [
         {"smiles": Chem.MolToSmiles(m), "explanation": ["amine-alcohol"]}
          for m in alcohol_mols
     ]
 
     # replace amine with a halide
-    halide_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("NH2"), Chem.MolFromSmiles("F"))
+    halide_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("[N;H2;X3]"), Chem.MolFromSmiles("F"))
     options += [
         {"smiles": Chem.MolToSmiles(m), "explanation": ["amine-halide"]}
          for m in halide_mols
@@ -123,7 +126,7 @@ def generate_option_primary_amine(mol: Chem.Mol) -> list[dict]:
 
 
 def generate_option_secondary_amine(mol: Chem.Mol) -> list[dict]:
-    amine_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("[NC2]H"))
+    amine_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("[N;H1;X3]"))
 
     if not amine_matches:
         return []
@@ -137,7 +140,7 @@ def generate_option_secondary_amine(mol: Chem.Mol) -> list[dict]:
     ]
 
     # replace amine with an ether
-    ether_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("[NC2]H"), Chem.MolFromSmiles("O"))
+    ether_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("[N;H1;X3]"), Chem.MolFromSmiles("O"))
     options += [
         {"smiles": Chem.MolToSmiles(m), "explanation": ["secondary-amine-ether"]}
          for m in ether_mols
@@ -255,13 +258,13 @@ def generate_option_nitrile(mol: Chem.Mol) -> list[dict]:
     return options
 
 def generate_option_thiol(mol: Chem.Mol) -> list[dict]:
-    thiol_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("SH"))
+    thiol_matches = mol.GetSubstructMatches(Chem.MolFromSmarts("[#16X2H]"))
 
     if not thiol_matches:
         return []
     
     # replace thiol with a chloride
-    chloride_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("SH"), Chem.MolFromSmiles("Cl"))
+    chloride_mols = rdmolops.ReplaceSubstructs(mol, Chem.MolFromSmarts("[#16X2H]"), Chem.MolFromSmiles("Cl"))
     options = [
         {"smiles": Chem.MolToSmiles(m), "explanation": ["thiol-halide"]}
          for m in chloride_mols
@@ -276,12 +279,18 @@ def generate_mcq_options(mol: Chem.Mol, num_options: int = 3) -> list[dict]:
     options_raw = []
 
     options_raw += generate_option_halide(mol)
+    options_raw += generate_option_alcohol(mol)
+    options_raw += generate_option_primary_o(mol)
+    options_raw += generate_option_primary_amine(mol)
+    options_raw += generate_option_secondary_amine(mol)
     options_raw += generate_option_carbonyl(mol)
     options_raw += generate_option_ether(mol)
     options_raw += generate_option_ester(mol)
     options_raw += generate_option_carboxylic_acid(mol)
+    options_raw += generate_option_nitrile(mol)
+    options_raw += generate_option_thiol(mol)
 
-    options = merge_duplicate_options(options_raw)
+    options = merge_duplicate_options(mol, options_raw)
 
     if len(options) >= num_options:
         sample = random.sample(options, num_options)
@@ -289,7 +298,7 @@ def generate_mcq_options(mol: Chem.Mol, num_options: int = 3) -> list[dict]:
     
     else:
         extra_options = generate_backup_options(mol, num_options)
-        options_merged = merge_duplicate_options(extra_options + options)
+        options_merged = merge_duplicate_options(mol, extra_options + options)
         sample = random.sample(options_merged, num_options)
         return sample
     
@@ -340,13 +349,17 @@ def generate_backup_options(mol: Chem.Mol, num_options: int) -> list[dict]:
     return top_options
 
 
-def merge_duplicate_options(options_raw: list[dict]) -> list[dict]:
+def merge_duplicate_options(original_mol: Chem.Mol, options_raw: list[dict]) -> list[dict]:
     """
     Merges duplicate options in the list of raw options. Combines the eligible explanations, and discards low-priority explanations (e.g. similarity-algo).
     """
 
     options = []
     for option in options_raw:
+        # remove the option if it's the same as the original molecule
+        if option["smiles"] == Chem.MolToSmiles(original_mol):
+            continue
+
         # add option if it's not already in the list
         if option["smiles"] not in [o["smiles"] for o in options]:
             options.append(option)
@@ -357,6 +370,11 @@ def merge_duplicate_options(options_raw: list[dict]) -> list[dict]:
             
             # find the existing option and merge the explanations
             existing_option = next(o for o in options if o["smiles"] == option["smiles"])
-            existing_option["explanation"] += option["explanation"]
+
+            # replace the similarity algorithm explanation if the new option has a better explanation
+            if existing_option["explanation"] == ["similarity-algo"]:
+                existing_option["explanation"] = option["explanation"]
+            elif option["explanation"] not in existing_option["explanation"]:
+                existing_option["explanation"] += option["explanation"]
 
     return options
